73fc22e57ead0c4995cd63aea3172a05a2a81d03
Aaron Minkov <aaronminkov@Aarons-MacBook-Pro.local>
2021-03-01 23:31:46 -0800

    Edit config

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php 
 	session_start();
 	// connect to database
-	$conn = mysqli_connect("localhost", "bruicypz_admin", "dARIEsSaHFRI", "bruicypz_bprjournal");
+	$conn = mysqli_connect("localhost", "*USERNAME*", "*PASSWORD*", "*DATABASE");
 
 	if (!$conn) {
 		die("Error connecting to database: " . mysqli_connect_error());
